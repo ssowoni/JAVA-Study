28d820b0194b71024ca0e710b3e251538309fc52
sowon <thdnjs0946@naver.com>
2023-08-07 22:26:45 +0900

    [2023-08-07] 메인라인 자바

---
+++ src/etc/CastingEx.java
@@ -0,0 +1,13 @@
+package etc;
+
+public class CastingEx {
+
+    public static void main(String[] args) {
+
+
+        String s_num = "10";
+        String StringNum = "12";
+        System.out.println("StringNum = " + StringNum.toString());
+
+    }
+}
